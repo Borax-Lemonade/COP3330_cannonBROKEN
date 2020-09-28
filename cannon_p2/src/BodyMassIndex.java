4d83addaf1cd7624f7578735af1706cb5103d566
Borax-Lemonade <rebeccacan222222@gmail.com>
2020-09-28 19:08:11 -0400

    minor changes

--- cannon_p2/src/BodyMassIndex.java
+++ cannon_p2/src/BodyMassIndex.java
@@ -1,16 +1,19 @@
-import java.math.*;
-import java.util.Scanner;
-
 public class BodyMassIndex {
     //public static boolean bmiInput;
     public static double bmiInput;
     public static String bmiCategory = "default";
 
     public BodyMassIndex(double height, double weight) {
+       calculateBMI(height, weight);
+       determineCategory();
+    }
+
+    public static void calculateBMI(double height, double weight) {
         bmiInput = (703*weight)/(Math.pow(height,2));
         bmiInput = Math.round(bmiInput*10);
         bmiInput /= 10;
-
+    }
+    public static void determineCategory() {
         if (bmiInput < 18.5)
             bmiCategory = "Underweight";
         if (bmiInput >= 18.5 && bmiInput < 24.9)
@@ -21,9 +24,4 @@ public class BodyMassIndex {
             bmiCategory = "Obesity";
     }
 
-    public static void main(String[] args) {
-        BodyMassIndex obj = new BodyMassIndex(62,114);
-        System.out.println(obj.bmiInput);
-        System.out.print(" " + obj.bmiCategory);
-    }
 }
